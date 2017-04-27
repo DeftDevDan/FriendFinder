ce886866622aba006c4ff67ee1c1088c161f3fcb
Daniel Kang <mrdanielkang@gmail.com>
2017-04-27 01:50:42 -0700

    Finish hw

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,51 @@
+var friendJSON = require('../data/friends.json');
+friends = friendJSON.friends;
+var path = require('path');
+var fs = require('fs');
 
+module.exports = function(app) {
+	app.post("/api/friends", function(req, res) {
+		var me = req.body;
+		var totalDifferences = [];
+		var bestMatch = 9000;
+		var bestMatchVar = [];
 
-app.post("/api/friends", function(req, res) {
-	var me = req.body;
-	var bestMatch = [];
+		for (var i = 0; i < friends.length; i++) {
+			if(me.name != friends[i].name) {
+				var total = 0;
+				for(var j = 0; j < friends[i].scores.length; j++) {
+					total += Math.abs(parseInt(friends[i].scores[j]) - parseInt(me.scores[j]));
+				}
 
-	//compare all friends data to my data
+				if(total <= bestMatch) {
+					if(total < bestMatch) {
+						bestMatchVar = [i];
+						bestMatch = total;
+					} else if(total === bestMatch) {
+						bestMatchVar.push(i);
+					}
+				}
+			}
+			totalDifferences.push(total);
+		}
 
-	friends.push(me);
+		updateJSON(me);
+		var rand = Math.floor(Math.random() * bestMatchVar.length);
+		res.json(friends[bestMatchVar[rand]]);
+	});	
 
-	res.json(bestMatch);
-});	
+	app.get("/api/friends", function(req, res) {
+		res.sendFile(path.join(__dirname, "../data/friends.json"));
+	});
+}
 
-app.get("/api/friends", function(req, res) {
-	res.sendFile(path.join(__dirname, "app/data/friends.json"));
-});
+function updateJSON(add) {
+	friendJSON.friends.push(add);
+	var data = JSON.stringify(friendJSON);
+
+	fs.writeFile('./app/data/friends.json', data, function(err) {
+		if(err) {
+			console.log(err);
+		}
+	});
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,13 @@
-app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
+var path = require("path");
+var express = require ('express');
+var home = express();
 
-app.get("/survey", function(req, res) {
-	res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
+module.exports = function(app) {
+	app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
+	});
+
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+	});
+}
